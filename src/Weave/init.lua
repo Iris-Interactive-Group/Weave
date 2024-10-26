6d24f44464979e87ea4d4a4db0a36a4205fbed54
AzuuuDev <azurisdev@gmail.com>
2024-10-26 13:48:50 +0100

    Major changes to how Weave will be structured, added TODO list which is primarily for developers

---
+++ src/Weave/init.lua
@@ -0,0 +1,13 @@
+--[[
+
+Weave is a backend framework for Roblox, created by Iris Interactive tech team.
+
+Use this script to access the different Weave components. 
+]]
+
+return {
+    Server = script.WeaveServer, 
+    Client = script.WeaveClient,
+    Utils = script.WeaveUtils
+}
+
