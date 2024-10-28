4ae573e91e3b44dd78cd7858c7258e2e4fb01796
AzuuuDev <azurisdev@gmail.com>
2024-10-28 14:10:51 +0000

    Fixed recursive require within WeaveServer, also added .gitignore for testing purposes on code

--- src/Weave/WeaveServer/init.lua
+++ src/Weave/WeaveServer/init.lua
@@ -2,10 +2,13 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Modules
-local WEAVE = require(ReplicatedStorage:FindFirstChild("Weave"))
-local WeaveUtils = WEAVE.Utils
+local WEAVE = script.Parent -- The Weave module script
+local Utils = require(WEAVE.WeaveUtils)
 
 local Weave = {}
 
+function Weave.StartServer()
+    print("Server Started!")
+end
 
 return Weave
